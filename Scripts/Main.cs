ec652a87c8cafc131d2a6f871c1ba431c02218be
itorri <itori.ohm@gmail.com>
2021-03-08 14:40:31 +0300

    Add ui and music selection

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -7,7 +7,8 @@ public class Main : Spatial
 	float whileMoving = 0;
 	Vector2 moveDirection = new Vector2(0, 0);
 	float moveSpeed = 0.02f;
-	KinematicBody head;
+
+	Area head;
 
 	PackedScene obstacleScene;
 	KinematicBody obstacle;
@@ -23,34 +24,42 @@ public class Main : Spatial
 
 	public override void _Ready()
 	{
-		head = GetNode<KinematicBody>("KinBody");
+		head = GetNode<Area>("Head");
 		material = GD.Load<SpatialMaterial>("res://Materials/1.tres");
 		obstacleScene = GD.Load<PackedScene>("res://Scenes/ObstacleBody.tscn");
 		
-		GetNode<Timer>("Timer").Start();
-		GetNode<AudioStreamPlayer>("AudioStreamPlayer3D").Play();
+		
 	}
 
 
 	public override void _Process(float delta)
 	{
 		
-		moveDirection = new Vector2(-Convert.ToInt32(Input.IsActionPressed("ui_right")) + Convert.ToInt32(Input.IsActionPressed("ui_left")),
-									-Convert.ToInt32(Input.IsActionPressed("ui_down"))  + Convert.ToInt32(Input.IsActionPressed("ui_up")));
+		moveDirection = new Vector2
+			(
+				-Convert.ToInt32(Input.IsActionPressed("ui_right")) + Convert.ToInt32(Input.IsActionPressed("ui_left")),
+				-Convert.ToInt32(Input.IsActionPressed("ui_down"))  + Convert.ToInt32(Input.IsActionPressed("ui_up"))
+			);
 
 		
 		moveDirection = moveDirection.Normalized();
 
-		moveDirection = new Vector2(moveDirection.x * 0.86f, 	
-									moveDirection.y * 0.86f);
+		moveDirection = new Vector2
+		(
+			moveDirection.x, 	
+			moveDirection.y
+		)*0.86f;
 
 		if (moveDirection != new Vector2(0, 0))
 		{
 			whileMoving += delta;
 			moveSpeed = RangeLerp(whileMoving, 0, 0.2f, 0.02f, 0.025f);
-			head.Translation = new Vector3(Mathf.MoveToward(head.Translation.x, moveDirection.x, moveSpeed),
-																			  Mathf.MoveToward(head.Translation.y, moveDirection.y, moveSpeed),
-																			  0);
+			head.Translation = new Vector3
+			(
+				Mathf.MoveToward(head.Translation.x, moveDirection.x, moveSpeed),
+				Mathf.MoveToward(head.Translation.y, moveDirection.y, moveSpeed),
+				0
+			);
 			
 		} else whileMoving -=delta;
 
@@ -58,28 +67,49 @@ public class Main : Spatial
 		
 		spectrum = (AudioEffectSpectrumAnalyzerInstance)AudioServer.GetBusEffectInstance(0, 0);
 
-		high = RangeLerp(GD.Linear2Db(spectrum.GetMagnitudeForFrequencyRange(7000, 16000).Length()),-60, 10, 0, 255);
-		mid = RangeLerp(GD.Linear2Db(spectrum.GetMagnitudeForFrequencyRange(500, 8000).Length()), -60,10,0,255);
-		low = RangeLerp(GD.Linear2Db(spectrum.GetMagnitudeForFrequencyRange(30, 100).Length()), -60,10,0,255);
+		high = RangeLerp(GD.Linear2Db(spectrum.GetMagnitudeForFrequencyRange(7000, 16000).Length()),-30, 10, 0, 255);
+		mid = RangeLerp(GD.Linear2Db(spectrum.GetMagnitudeForFrequencyRange(500, 8000).Length()), -30,10,0,255);
+		low = RangeLerp(GD.Linear2Db(spectrum.GetMagnitudeForFrequencyRange(30, 100).Length()), -30,10,0,255);
 		
-		material.AlbedoColor = Color.Color8((byte)high, (byte)mid, (byte)low, 255);
+		material.AlbedoColor = Color.Color8
+		(
+			(byte)high, 
+			(byte)mid, 
+			(byte)low, 
+			255
+		);
 
 		time += delta;
 
-	}
-
-	public void _on_RigidBody_body_entered(Node body){
-		GD.Print(body);
 	}
 	public void _on_Timer_timeout()
 	{
 		Spatial spatial = GetNode<Spatial>("Spatial/Tunnel/Spatial");
 		obstacle = (KinematicBody)obstacleScene.Instance();
 		AddChild(obstacle);
-		obstacle.Translation = new Vector3(0,0,55);
+		obstacle.Translation = new Vector3(0,0,26);
 	}
 	public float RangeLerp(float value, float istart, float istop, float ostart, float ostop)
 	{
 		return ostart + (ostop - ostart) * value / (istop - istart);
+	}
+
+	public void _on_ObstacleKiller_body_entered(Node body)
+	{
+		body.QueueFree();
+	}
+	public void _on_Head_body_entered(Node body)
+	{
+		GetTree().ReloadCurrentScene();
+	}
+	public void _on_ItemList_item_selected(int index)
+	{
+
+		var itemList = GetNode<ItemList>("Control/ItemList");
+		itemList.Hide();
+
+		GetNode<Timer>("Timer").Start();
+		GetNode<AudioStreamPlayer>("AudioStreamPlayer3D").Stream = GD.Load<AudioStream>("res://Audio/" + itemList.GetItemText(index) + ".ogg");
+		GetNode<AudioStreamPlayer>("AudioStreamPlayer3D").Play();
 	}
 }	
