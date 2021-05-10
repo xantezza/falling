4f8135b9a85e4c6f7d829617a9a394c2cd90b84b
itorri <itori.ohm@gmail.com>
2021-05-10 14:53:23 +0300

    code refactoring with some functional changes

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -5,19 +5,19 @@ public class Main : Spatial
 {
 	public float time = 0;
 	public float trackLenght = 100;
-	float moveSpeed = 0.03f;
+	float moveSpeed = 0.02f;
 	float high;
 	float mid;
-	float low;
+	float low;	
 
 	float health = 100;
-	string currentOS;
 	float bottomDb = -30;
 	ProgressBar healthBar;
 	ProgressBar progressBar;
 	Area head;
 	PackedScene obstacleScene;
 	KinematicBody obstacle;
+	Timer timer;
 	SpatialMaterial material;
 	AudioEffectSpectrumAnalyzerInstance spectrum;
 	Vector2 moveDirection = new Vector2(0, 0);
@@ -27,23 +27,17 @@ public class Main : Spatial
 		head = GetNode<Area>("Head");
 		healthBar = GetNode<ProgressBar>("Control/Health");
 		progressBar = GetNode<ProgressBar>("Control/Progress");
-
-
 		material = GD.Load<SpatialMaterial>("res://Materials/1.tres");
 		obstacleScene = GD.Load<PackedScene>("res://Scenes/ObstacleBody.tscn");
-
-		currentOS = OS.GetName();
+		timer = GetNode<Timer>("Spawner");	
 	}
 
 	public override void _PhysicsProcess(float delta)
 	{
-		if (currentOS == "Windows") moveDirection = new Vector2(-Convert.ToInt32(Input.IsActionPressed("ui_right")) + Convert.ToInt32(Input.IsActionPressed("ui_left")),
-																-Convert.ToInt32(Input.IsActionPressed("ui_down")) + Convert.ToInt32(Input.IsActionPressed("ui_up"))
-																);
-
-
-		//if (currentOS == "Android") moveDirection = new Vector2(Input.GetAccelerometer().x,Input.GetAccelerometer().y);
-
+		moveDirection = new Vector2(
+			-Convert.ToInt32(Input.IsActionPressed("ui_right")) + Convert.ToInt32(Input.IsActionPressed("ui_left")),
+			-Convert.ToInt32(Input.IsActionPressed("ui_down")) + Convert.ToInt32(Input.IsActionPressed("ui_up"))
+			);
 		if (moveDirection != new Vector2(0, 0))
 		{
 			moveDirection = moveDirection.Normalized();
@@ -58,7 +52,7 @@ public class Main : Spatial
 			{
 				head.Translation += new Vector3( moveDirection.x,
 												 moveDirection.y,
-												 0);
+												 0)*delta*100;
 			}
 			else
 			{
@@ -68,50 +62,57 @@ public class Main : Spatial
 			}
 		}
 
+		
+		material.AlbedoColor = MusicToRGB();
+		time += delta;
+		
+		healthBar.Value = Mathf.MoveToward((float)healthBar.Value, health, 0.5f);
+		if (Input.IsActionJustPressed("ui_cancel"))
+			GetTree().Quit();
+	}
+	public Color MusicToRGB(){
+
 		spectrum = (AudioEffectSpectrumAnalyzerInstance)AudioServer.GetBusEffectInstance(0, 0);
 		high = RangeLerp(GD.Linear2Db(spectrum.GetMagnitudeForFrequencyRange(5000, 20000).Length()), bottomDb, 10, 0, 255);
 		mid = RangeLerp(GD.Linear2Db(spectrum.GetMagnitudeForFrequencyRange(500, 5000).Length()), bottomDb, 10, 0, 255);
 		low = RangeLerp(GD.Linear2Db(spectrum.GetMagnitudeForFrequencyRange(10, 200).Length()), bottomDb, 0, 0, 255);
 
-		material.AlbedoColor = Color.Color8
+		return Color.Color8
 		(
 			(byte)high,
 			(byte)mid,
 			(byte)low,
 			255
 		);
-
-		time += delta;
-		
-		healthBar.Value = Mathf.MoveToward((float)healthBar.Value, health, 0.5f);
-		if (Input.IsActionJustPressed("ui_cancel")) GetTree().Quit();
 	}
-	public float RangeLerp(float value, float istart, float istop, float ostart, float ostop)
+	public static float RangeLerp(float value, float istart, float istop, float ostart, float ostop)
 	{
 		return ostart + (ostop - ostart) * value / (istop - istart);
 	}
-	void _on_Timer_timeout()
+	void _on_Spawner_timeout()
 	{
 		Spatial spatial = GetNode<Spatial>("Spatial/Tunnel/Spatial");
 		obstacle = (KinematicBody)obstacleScene.Instance();
 		AddChild(obstacle);
 		obstacle.Translation = new Vector3(0, 0, 52);
-		progressBar.Value += 0.5f;
-		if (progressBar.Value > progressBar.MaxValue) GetTree().ReloadCurrentScene();
 
+		var timer = GetNode<Timer>("Spawner");
+		timer.WaitTime = RangeLerp(time, 1, trackLenght, 0.5f, 0.2f);
+		Console.WriteLine(timer.WaitTime);
+		if (progressBar.Value > progressBar.MaxValue) GetTree().ReloadCurrentScene();
+		progressBar.Value += timer.WaitTime;
 	}
-	
+
 	void _on_ObstacleKiller_body_entered(Node body)
 	{
+		GetNode<Label>("Control/Label2").Text += "1";
 		body.QueueFree();
 	}
 	void _on_Head_body_entered(Node body)
 	{
 		health -= 20;
-		
 		if (health < 0) GetTree().ReloadCurrentScene();
 	}
-	
 	void _on_ItemList_item_selected(int index){
 
 		ItemList itemList = GetNode<ItemList>("Control/ScrollContainer/ItemList");
@@ -121,12 +122,11 @@ public class Main : Spatial
 		
 		audioPlayer.Stream = GD.Load<AudioStream>("res://Audio/" + itemList.GetItemText(index) + ".ogg");
 		audioPlayer.Play();
+		trackLenght = audioPlayer.Stream.GetLength();
+		progressBar.MaxValue = trackLenght;
 
-		progressBar.MaxValue = audioPlayer.Stream.GetLength();
-		
-		
 		GetNode<Label>("Control/Label").Hide();
-		GetNode<Timer>("Timer").Start();
+		GetNode<Timer>("Spawner").Start();
 	}
 	void _on_VSlider_value_changed(float value){
 		bottomDb = value;
--- Scripts/ObstacleBody.cs
+++ Scripts/ObstacleBody.cs
@@ -1,5 +1,4 @@
 using Godot;
-using System;
 
 public class ObstacleBody : KinematicBody
 {	
@@ -10,20 +9,35 @@ public class ObstacleBody : KinematicBody
 		GD.Randomize();
 		float time = GetParent<Main>().time;
 		float trackLenght = GetParent<Main>().trackLenght;
-		velocity = new Vector3(0, 0, -0.15f) * GetParent<Main>().RangeLerp(time, 0, trackLenght, 0.5f, 3f);
+		velocity = new Vector3(
+			0, 
+			0, 
+			-0.08f * Main.RangeLerp(time, 0, trackLenght, 3f, 5f)
+			);
 		
-		GetNode<CollisionShape>("1").RotationDegrees += new Vector3(0, 0, (float)GD.RandRange(0,360));
-		RotationDegrees += new Vector3(0, 0, (float)GD.RandRange(0, 360));
+		GetNode<CollisionShape>("1").RotationDegrees += new Vector3(
+			0, 
+			0, 
+			(float)GD.RandRange(0,360)
+			);
 
-		float obstacleOffset = 0;// GetParent<Main>().RangeLerp(time, 0, trackLenght, 2f, 0f); ;
+		RotationDegrees += new Vector3(
+			0,
+			0, 
+			(float)GD.RandRange(0, 360)
+			);
 
-		GetNode<CollisionShape>("1").Translation += new Vector3( (float)GD.RandRange(-obstacleOffset, obstacleOffset),
-																 (float)GD.RandRange(-obstacleOffset, obstacleOffset),
-																 0);
+		float obstacleOffset = 0;
+
+		GetNode<CollisionShape>("1").Translation += new Vector3( 
+			(float)GD.RandRange(-obstacleOffset, obstacleOffset),
+			(float)GD.RandRange(-obstacleOffset, obstacleOffset),
+			0
+			);
 		
 	}	
 	public override void _Process(float delta)
 	{
-		Translation += velocity ;
+		Translation += velocity*delta*100 ;
 	}
 }
